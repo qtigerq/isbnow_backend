31ded679dc1c9f153ddecef5305d46b6f10639ee
Tiago <chicobg@gmail.com>
2022-02-23 17:34:09 -0300

    ajustando o banco de dados para o banco na Heroku

--- server/infra/database.js
+++ server/infra/database.js
@@ -1,10 +1,21 @@
 const pgp = require('pg-promise')();    //Biblioteca para interagir com o banco de dados.
+
+/* DADOS PARA O BANCO VIA LOCALHOST
 const db = pgp({
     user: 'postgres',
     password: 'odiseia',                //nunca deixar a senha assim em producao
     host: 'localhost',
     port: 5432,
     database: 'postgres'
+})*/
+
+//DADOS PARA O BANCO VIA HEROKU
+const db = pgp({
+    user: 'noatiezqkiyoup',
+    password: '6806740ce79273ca07c973f9374902b4702d8efbeb307da925fe2bb282e6d507',
+    host: 'ec2-52-207-74-100.compute-1.amazonaws.com',
+    port: 5432,
+    database: 'd75g511sc2bs86'
 })
 
 module.exports = db;
