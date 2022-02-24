d01247c0996a634f7fc103ede00a908258a5a963
Tiago <chicobg@gmail.com>
2022-02-24 10:57:42 -0300

    ajustando path e pasta dos certificados

--- server/server.js
+++ server/server.js
@@ -61,18 +61,19 @@ if ( protocol === 'https' ) {
 	const { execSync } = require( 'child_process' );
 
 	const execOptions = { encoding: 'utf-8', windowsHide: true };
-	let key = './certs/key.pem';
-	let certificate = './certs/certificate.pem';
+	let key = './../certs/key.pem';
+  
+	let certificate = './../certs/certificate.pem';
 	
 	if (!fs.existsSync(key) || !fs.existsSync(certificate)) {
 		try {
 			execSync( 'openssl version', execOptions );
 			execSync(
-				`openssl req -x509 -newkey rsa:2048 -keyout ./certs/key.tmp.pem -out ${ certificate } -days 365 -nodes -subj "/C=US/ST=Foo/L=Bar/O=Baz/CN=localhost"`,
+				`openssl req -x509 -newkey rsa:2048 -keyout ./../certs/key.tmp.pem -out ${ certificate } -days 365 -nodes -subj "/C=US/ST=Foo/L=Bar/O=Baz/CN=localhost"`,
 				execOptions
 			);
-			execSync( `openssl rsa -in ./certs/key.tmp.pem -out ${ key }`, execOptions );
-			execSync( 'rm ./certs/key.tmp.pem', execOptions );
+			execSync( `openssl rsa -in ./../certs/key.tmp.pem -out ${ key }`, execOptions );
+			execSync( 'rm ./../certs/key.tmp.pem', execOptions );
 		} catch ( error ) {
 			console.error( error );
 		}
